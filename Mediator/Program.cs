3e9c2c7e67d8359c797378fc5fd6bd6d5f1e6ce0
ozgurruzgar <ozgurruzgar@users.noreply.github.com>
2023-03-16 12:20:18 +0300

    Added Mediator Pattern

---
+++ Mediator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Mediator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
