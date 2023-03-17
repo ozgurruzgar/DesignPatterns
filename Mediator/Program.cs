1518a1e29b589e32c6ba78b0add570442a847ac9
Özgür Rüzgar <94163712+ozgurruzgar@users.noreply.github.com>
2023-03-17 18:29:00 +0300

    Added Command and Visitor Pattern.

--- Mediator/Program.cs
+++ Mediator/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Mediator
 {
@@ -6,7 +7,94 @@ namespace Mediator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Mediator mediator = new Mediator();
+            Teacher teacher = new Teacher(mediator);
+            teacher.Name = "Engin";
+
+            mediator.Teacher = teacher;
+
+            Student student = new Student(mediator);
+            student.Name = "Derin";
+
+            Student student2 = new Student(mediator);
+            student2.Name = "Salih";
+
+            mediator.Students = new List<Student> {student,student2 };
+            teacher.RecieveQuestion("is it true?",student2);
+
+            teacher.SendNewImageUrl("slide1.jpg");
+        }
+    }
+    abstract class CourseMember
+    {
+        protected Mediator Mediator;
+
+        protected CourseMember(Mediator mediator)
+        {
+            Mediator = mediator;
+        }
+    }
+    class Teacher : CourseMember
+    {
+        public string Name { get; internal set; }
+
+        public Teacher(Mediator mediator) : base(mediator)
+        {
+
+        }
+        internal void RecieveQuestion(string question, Student student)
+        {
+            Console.WriteLine("Teacher recieved  a question from {0},{1}", student.Name, question);
+        }
+        public void SendNewImageUrl(string url)
+        {
+            Console.WriteLine("Teacher changed slide : {0}", url);
+            Mediator.UpdateImage(url);
+        }
+        public void AnswerQuestion(string answer, Student student)
+        {
+            Console.WriteLine("Teacher answered question {0},{1}", student.Name, answer);
+        }
+    }
+    class Student : CourseMember
+    {
+        public Student(Mediator mediator) : base(mediator)
+        {
+        }
+
+        public string Name { get; internal set; }
+
+        internal void RecieveImage(string url)
+        {
+            Console.WriteLine("{1} recieved image : {0}", url,Name);
+        }
+
+        internal void RecieveAnswer(string answer)
+        {
+            Console.WriteLine("Student recieved answer [0}", answer);
+        }
+    }
+    class Mediator
+    {
+        public Teacher Teacher { get; set; }
+        public List<Student> Students { get; set; }
+
+        public void UpdateImage(string url)
+        {
+            foreach (var student in Students)
+            {
+                student.RecieveImage(url);
+            }
+        }
+
+        public void SendQuestion(string question, Student student)
+        {
+            Teacher.RecieveQuestion(question, student);
+        }
+
+        public void SendAnswer(string answer, Student student)
+        {
+            student.RecieveAnswer(answer);
         }
     }
 }
---
+++ Visitor/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Visitor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Manager manager = new Manager {Name="Engin",Salary=1000 };
+            Manager manager2 = new Manager {Name="Salih",Salary=900 };
+
+            Worker worker = new Worker {Name="Derin",Salary=800 };
+            Worker worker2 = new Worker {Name="Ali",Salary=800 };
+
+            manager.Subordinates.Add(manager2);
+            manager2.Subordinates.Add(worker);
+            manager2.Subordinates.Add(worker2);
+
+            OrganisationalStructure organisationalStructure = new OrganisationalStructure(manager);
+
+            PayrollVisitor payrollVisitor = new PayrollVisitor();
+            PayriseVisitor payriseVisitor = new PayriseVisitor();
+
+            organisationalStructure.Accept(payrollVisitor);
+            organisationalStructure.Accept(payriseVisitor);
+
+            Console.ReadLine();
+        }
+    }
+
+    class OrganisationalStructure
+    {
+        public EmployeeBase Employee;
+
+        public OrganisationalStructure(EmployeeBase firstEmployee)
+        {
+            Employee = firstEmployee;
+        }
+
+        public void Accept(VisitorBase visitor)
+        {
+            Employee.Accept(visitor);
+        }
+    }
+    abstract class EmployeeBase
+    {
+        public abstract void Accept(VisitorBase visitor);
+        public string Name { get; set; }
+        public decimal Salary { get; set; }
+    }
+    class Manager : EmployeeBase
+    {
+        public Manager()
+        {
+            Subordinates = new List<EmployeeBase>();
+        }
+        public List<EmployeeBase> Subordinates { get; set; }
+        public override void Accept(VisitorBase visitor)
+        {
+            visitor.Visit(this);
+
+            foreach (var employee in Subordinates)
+            {
+                employee.Accept(visitor);
+            }
+        }
+    }
+    class Worker : EmployeeBase
+    {
+        public override void Accept(VisitorBase visitor)
+        {
+            visitor.Visit(this);
+        }
+    }
+    abstract class VisitorBase
+    {
+        public abstract void Visit(Worker worker);
+        public abstract void Visit(Manager manager);
+    }
+    class PayrollVisitor : VisitorBase
+    {
+        public override void Visit(Worker worker)
+        {
+            Console.WriteLine("{0} paid {1}",worker.Name,worker.Salary);
+        }
+
+        public override void Visit(Manager manager)
+        {
+            Console.WriteLine("{0} paid {1}", manager.Name, manager.Salary);
+        }
+    }
+    class PayriseVisitor : VisitorBase
+    {
+        public override void Visit(Worker worker)
+        {
+            Console.WriteLine("{0} salary increased to {1}", worker.Name, worker.Salary*(decimal)1.1);
+        }
+
+        public override void Visit(Manager manager)
+        {
+            Console.WriteLine("{0} salary increased to {1}", manager.Name, manager.Salary*(decimal) 1.2);
+        }
+    }
+}
